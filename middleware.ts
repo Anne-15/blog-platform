29d947b655e2034da4f58a5af2b071d314cc6869
Anne-15 <wariiyuanne@gmail.com>
2024-07-11 11:10:10 +0300

    changes to the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,25 @@
 'use server'
 import { NextRequest, NextResponse } from "next/server";
-import { getSession } from "./lib/auth";
+import { updateSession } from "./lib/auth";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession(request);
+  const session = request.cookies.get("session")?.value;
   const protectedRoutes = ["/Riishi/AddProject", "/Riishi/AddDesign"];
 
+  console.log(request);
   // Check if the requested route is protected and there is no session
   if (protectedRoutes.includes(request.nextUrl.pathname) && !session) {
     // Redirect to login page
     const loginUrl = new URL("/Login", request.url);
     loginUrl.searchParams.set("redirectTo", request.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
+    // const res = NextResponse.redirect(new URL("/Login", request.url));
+    // console.log(res);
+    // return res;
   }
-  return NextResponse.next();
+
+  // await updateSession(request);
+  return request.nextUrl
 }
 
 export const config = {
